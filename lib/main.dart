e1c4a105b4cfbf60d22e11d5794d4f8a53d8cb14
luqman jaffary <luqmanjaffary79@gmail.com>
2025-04-04 21:14:02 +0500

    start main file for the app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import './screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
