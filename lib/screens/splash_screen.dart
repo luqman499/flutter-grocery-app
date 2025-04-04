8413fef30b923f51f7a2d80ecfdc85af8117113f
luqman jaffary <luqmanjaffary79@gmail.com>
2025-04-04 23:48:56 +0500

    start main file for the app

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -9,7 +10,23 @@ class SplashScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Center(child: Image.asset('images/splash_image.png'))],
+        children: [
+          Center(
+            child: Image.asset(
+              'images/splash_image.png',
+              height: 80,
+              width: 80,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Grocery Plus',
+            style: GoogleFonts.poppins(
+              fontSize: 20,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ],
       ),
     );
   }
