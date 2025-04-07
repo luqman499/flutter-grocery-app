56d9e93382c0af2243a288b3fdc777b2cf873278
luqman jaffary <luqmanjaffary79@gmail.com>
2025-04-08 01:19:04 +0500

    complete the login page

--- lib/widgets/primary_button.dart
+++ lib/widgets/primary_button.dart
@@ -33,3 +33,48 @@ class PrimaryButton extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+//
+// class PrimaryButton extends StatelessWidget {
+//   final String title;
+//   final IconData icon;
+//   final Function() ontap;
+//
+//   const PrimaryButton({
+//     super.key,
+//     required this.title,
+//     required this.icon,
+//     required this.ontap,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: ontap,
+//       child: Container(
+//         height: 58,
+//         width: double.infinity,
+//         decoration: BoxDecoration(
+//           color: Color(0xff5EC401),
+//           borderRadius: BorderRadius.circular(6),
+//         ),
+//         child: Center(
+//           child: Row(
+//             mainAxisSize: MainAxisSize.min,
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 title,
+//                 style: GoogleFonts.poppins(color: Colors.white, fontSize: 16),
+//               ),
+//               const SizedBox(width: 8),
+//               Icon(icon, color: Colors.white),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
