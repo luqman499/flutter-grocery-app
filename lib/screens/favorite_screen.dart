07b787a167ca1b76f72e84004f644a5a9728c42b
luqman jaffary <luqmanjaffary79@gmail.com>
2025-04-11 23:45:04 +0500

    add favorite section

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_plus/widgets/favorite_card_widget.dart';
 
 class FavoriteScreen extends StatelessWidget {
   const FavoriteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Favorite Scrren'));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favorite Screen')),
+      body: Padding(
+        padding: EdgeInsets.only(left: 16.0, right: 16.0),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return FavoriteCardWidget();
+          },
+        ),
+      ),
+    );
   }
 }
